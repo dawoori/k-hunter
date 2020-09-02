49ec4cadec18ae5f054a6b87aaac25fac48e364c
dawoori <daul@kookmin.ac.kr>
2020-09-02 19:06:01 +0900

    final distribute

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# k-hunter
+# 국민대학교 수강신청 알리미 K-Hunter
+수강신청을 원하는 과목이 열렸는지 Slack 채널로 알려드립니다.
+
+## How to run
+- `src/index.js`에서 `LECTURE_CODE`와 `LECTURE_NAME`을 원하는 과목 코드와 이름으로 설정(순서 일치해야함)
+
+### How to run(Github Actions)
+- Fork repository
+- `Settings` - `Secrets` - `Add a new secret` 메뉴로 진입
+- `ID`, `PASSWS`, `WEBHOOKS`를 각각 종합정보시스템 id, password, 슬랙 봇 Incommig Webhooks 주소로 저장
+
+### How to run(local)
+- `src/index.js`에서 `ID`, `PASSWD`를 종합정보시스템 id와 password로 설정
+- `src/index.js`에서 `WEBHOOKS`를 슬랙 봇 Incommig Webhooks 주소로 설정
+- `npm run build`
+- `npm start`
--- src/index.js
+++ src/index.js
@@ -26,18 +26,17 @@ const LECTURE_NAME = ["소프트웨어디자인패턴", "네트워크서비스�
     const mainFrame = (await page.frames())[3];
 
     await leftFrame.evaluate(() => { document.querySelector('#수강정보').click(); });
-    // await leftFrame.evaluate(() => { document.querySelector('#수강정보Usb0101e').click(); });
-    await leftFrame.evaluate(() => { document.querySelector('#수강정보Usb0902e').click(); });
+    await leftFrame.evaluate(() => { document.querySelector('#수강정보Usb0101e').click(); });
     await page.waitFor(2000);
 
     const lectureList = (await page.frames())[6];
 
     for(let i = 0; i < LECTURE_CODE.length; i++) {
-      // await mainFrame.evaluate(() => { document.querySelector('body > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr > td:nth-child(7) > input[type=radio]:nth-child(2)').click(); });
-      // await mainFrame.evaluate((code, i) => { document.querySelector('#layer1 > form > table > tbody > tr > td:nth-child(2) > input[type=text]').value = code[i]; }, LECTURE_CODE, i);
-      // await mainFrame.evaluate(() => { document.querySelector('body > table > tbody > tr > td > table:nth-child(5) > tbody > tr > td:nth-child(2) > a').click(); });
+      await mainFrame.evaluate(() => { document.querySelector('body > table > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(1) > table > tbody > tr > td:nth-child(7) > input[type=radio]:nth-child(2)').click(); });
+      await mainFrame.evaluate((code, i) => { document.querySelector('#layer1 > form > table > tbody > tr > td:nth-child(2) > input[type=text]').value = code[i]; }, LECTURE_CODE, i);
+      await mainFrame.evaluate(() => { document.querySelector('body > table > tbody > tr > td > table:nth-child(5) > tbody > tr > td:nth-child(2) > a').click(); });
     
-      // await page.waitFor(1000);
+      await page.waitFor(1000);
 
       const lectureString = await lectureList.evaluate(() => {
         const cName = document.querySelector('body > table').childNodes[1].children[0].children[3].outerHTML;
